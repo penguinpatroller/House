e8884551b889a531711e379c3bde2a3e3ddb7124
penguinpatroller <camdav@umich.edu>
2019-07-31 13:36:12 -0500

    Added rgb_led lib to house

--- House.h
+++ House.h
@@ -33,7 +33,37 @@ private:
   const int pir_pin;
 };
 
+//RGB class
+class rgb_led
+{
+  public:
+    rgb_led(const int &r, const int &g, const int &b)
+    : red_pin(r), green_pin(g), blue_pin(b)
+    {
+      Serial.println("constructor");
+      pinMode(r, OUTPUT);
+      pinMode(g, OUTPUT);
+      pinMode(b, OUTPUT);
+    }
 
+    void glow(const int &r, const int &g, const int &b) const
+    {
+      analogWrite(red_pin, r);
+      analogWrite(green_pin, g);
+      analogWrite(blue_pin, b);
+    }
+
+    void turn_off()const
+    {
+      glow(0,0,0);
+    }
+
+  private:
+    const int red_pin;
+    const int green_pin;
+    const int blue_pin;
+
+};
 
 
 #endif
